92888e8cf278f2c10fc52764da2e715c60ddad12
Wikan-dev <oneshootindo@gmail.com>
2025-08-14 10:05:45 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './BankApp', // Ensures that assets are correctly linked
+  base: './BankAppReact', // Ensures that assets are correctly linked
 })
